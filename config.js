2ec1ab2b70decb337f64192ad206c01cf3aa1caf
PSquare18 <162063049+PSquare18@users.noreply.github.com>
2024-03-12 15:08:48 -1100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
+  ['233535990156', 'P_Squar18', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
